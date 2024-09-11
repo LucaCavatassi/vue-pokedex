c5d3838f27d2f07f306cb50a213a85638648f249
LucaCavatassi <lucamariacavatassi@gmail.com>
2024-09-11 11:25:32 +0200

    feat: font for the whole project

--- src/App.vue
+++ src/App.vue
@@ -5,5 +5,10 @@
   <h1>Hello World</h1>
 </template>
 
-<style scoped lang="scss">
+<style lang="scss">
+  @import url('https://fonts.googleapis.com/css2?family=Electrolize&display=swap');
+
+  html, body {
+  font-family: 'Electrolize', sans-serif;
+}
 </style>
